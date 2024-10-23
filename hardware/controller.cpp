fceefc1d74f925d6ae3ed7e0709e1395b3ff0444
Haruki Isono <haruki.isono861@gmail.com>
2024-10-23 19:02:40 +0900

    fix: simple_listener

--- hardware/controller.cpp
+++ hardware/controller.cpp
@@ -1,222 +1,152 @@
-#include "lucia_controller/controller.hpp"
-
-#include <chrono>
-#include <cmath>
-#include <cstddef>
-#include <limits>
-#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <yarp/os/all.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Port.h>
+#include <yarp/os/Bottle.h>
+#include <std_msgs/msg/float64_multi_array.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <vector>
-#include <tf2/LinearMath/Quaternion.h>
 
-#include "hardware_interface/types/hardware_interface_type_values.hpp"
-#include "rclcpp/rclcpp.hpp"
-
-namespace robot_controller
+class YarpReceiver : public rclcpp::Node
 {
-
-    hardware_interface::CallbackReturn RobotControllerHardware::on_init(
-        const hardware_interface::HardwareInfo &info)
+public:
+    YarpReceiver()
+    : Node("yarp_receiver")
     {
-        if (
-            hardware_interface::SystemInterface::on_init(info) !=
-            hardware_interface::CallbackReturn::SUCCESS)
-        {
-            return hardware_interface::CallbackReturn::ERROR;
-        }
-        port = (info_.hardware_parameters["serial_port"]);
-        RCLCPP_INFO(rclcpp::get_logger("RobotControllerHardware"), "Hardware parameter - port: %s", port.c_str());
-        rclcpp::on_shutdown(std::bind(&RobotControllerHardware::stop, this));
-        return hardware_interface::CallbackReturn::SUCCESS;
+        RCLCPP_INFO(this->get_logger(), "YARP Receiver Node has been started.");
+        
+        // Initialize YARP network
+        yarp::os::Network yarp;
+
+        // Open YARP port
+        port.open("/receiver");
+        p_cmd.open("/remoteController/remote:o");   //motor command
+        p_tou.open("/remoteController/remote:i");   //encoder reading
+
+        // Connect to the sender port
+        yarp::os::Network::connect("/sender", "/receiver");
+        yarp::os::Network::connect("/vehicleDriver/remote:i", "/remoteController/remote:i");  //motor command
+        yarp::os::Network::connect("/vehicleDriver/encoder:o", "/remoteController/remote:o");  //encoder reading
+        
+        // Create a timer to periodically check for messages
+        timer_ = this->create_wall_timer(
+            std::chrono::seconds(1),
+            std::bind(&YarpReceiver::receive_message, this)
+        );
+
+        // Initialize ROS publisher
+        publisher_ = this->create_publisher<std_msgs::msg::String>("yarp_to_ros", 50);
+        odom_publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("odometry", 50); //odometry
+        // cmd_vel subscriber
+        cmd_subscriber_ = this->create_subscription<geometry_msgs::msg::Twist>(
+            "cmd_vel", 50, std::bind(&YarpReceiver::cmd_callback, this, std::placeholders::_1)
+        );
     }
 
-    hardware_interface::CallbackReturn RobotControllerHardware::on_configure(
-        const rclcpp_lifecycle::State & /*previous_state*/)
+    /*~YarpReceiver()
     {
-        hardware_handler = new HardwareHandler(port);
-        hardware_handler->init();
-        RCLCPP_INFO(
-            rclcpp::get_logger("RobotControllerHardware"), "Successfully configured!");
+        port.close();
+        p_cmd.close();
+        p_tou.close();
+    }*/
 
-        return hardware_interface::CallbackReturn::SUCCESS;
+    void close_ports()
+    {
+        port.close();
+        p_cmd.close();
+        p_tou.close();
     }
 
-    std::vector<hardware_interface::StateInterface> RobotControllerHardware::export_state_interfaces()
+private:
+    void receive_message()
     {
-        std::vector<hardware_interface::StateInterface> state_interfaces;
-
-        for (auto i = 0u; i < info_.joints.size(); i++)
+        yarp::os::Bottle bot;
+        if (port.read(bot))
         {
-            if (info_.joints[i].name.find("left") != std::string::npos)
-            {
-                state_interfaces.emplace_back(hardware_interface::StateInterface(
-                    info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_positions_left));
-                state_interfaces.emplace_back(hardware_interface::StateInterface(
-                    info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &hw_velocities_left));
-            }
-            if (info_.joints[i].name.find("right") != std::string::npos)
-            {
-                state_interfaces.emplace_back(hardware_interface::StateInterface(
-                    info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_positions_right));
-                state_interfaces.emplace_back(hardware_interface::StateInterface(
-                    info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &hw_velocities_right));
-            }
-        }
+            RCLCPP_INFO(this->get_logger(), "Received message: %s", bot.toString().c_str());
 
-        // export sensor state interface
-        for (uint i = 0; i < info_.sensors[0].state_interfaces.size(); i++)
-        {
-            // state_interfaces.emplace_back(hardware_interface::StateInterface(
-            //     info_.sensors[0].name, info_.sensors[0].state_interfaces[i].name, &hw_sensor_states_[i]));
-            if (info_.sensors[0].state_interfaces[i].name.find("linear_acceleration.x") != std::string::npos)
-            {
-                state_interfaces.emplace_back(hardware_interface::StateInterface(
-                    info_.sensors[0].name, info_.sensors[0].state_interfaces[i].name, &hw_sensor_states_accel_x));
-            }
-            if (info_.sensors[0].state_interfaces[i].name.find("linear_acceleration.y") != std::string::npos)
-            {
-                state_interfaces.emplace_back(hardware_interface::StateInterface(
-                    info_.sensors[0].name, info_.sensors[0].state_interfaces[i].name, &hw_sensor_states_accel_y));
-            }
-            if (info_.sensors[0].state_interfaces[i].name.find("linear_acceleration.z") != std::string::npos)
-            {
-                state_interfaces.emplace_back(hardware_interface::StateInterface(
-                    info_.sensors[0].name, info_.sensors[0].state_interfaces[i].name, &hw_sensor_states_accel_z));
-            }
-            if (info_.sensors[0].state_interfaces[i].name.find("angular_velocity.x") != std::string::npos)
-            {
-                state_interfaces.emplace_back(hardware_interface::StateInterface(
-                    info_.sensors[0].name, info_.sensors[0].state_interfaces[i].name, &hw_sensor_states_gyro_x));
-            }
-            if (info_.sensors[0].state_interfaces[i].name.find("angular_velocity.y") != std::string::npos)
-            {
-                state_interfaces.emplace_back(hardware_interface::StateInterface(
-                    info_.sensors[0].name, info_.sensors[0].state_interfaces[i].name, &hw_sensor_states_gyro_y));
-            }
-            if (info_.sensors[0].state_interfaces[i].name.find("angular_velocity.z") != std::string::npos)
-            {
-                state_interfaces.emplace_back(hardware_interface::StateInterface(
-                    info_.sensors[0].name, info_.sensors[0].state_interfaces[i].name, &hw_sensor_states_gyro_z));
-            }
-            if (info_.sensors[0].state_interfaces[i].name.find("orientation.x") != std::string::npos)
-            {
-                state_interfaces.emplace_back(hardware_interface::StateInterface(
-                    info_.sensors[0].name, info_.sensors[0].state_interfaces[i].name, &hw_sensor_states_orientation_x));
-            }
-            if (info_.sensors[0].state_interfaces[i].name.find("orientation.y") != std::string::npos)
-            {
-                state_interfaces.emplace_back(hardware_interface::StateInterface(
-                    info_.sensors[0].name, info_.sensors[0].state_interfaces[i].name, &hw_sensor_states_orientation_y));
-            }
-            if (info_.sensors[0].state_interfaces[i].name.find("orientation.z") != std::string::npos)
-            {
-                state_interfaces.emplace_back(hardware_interface::StateInterface(
-                    info_.sensors[0].name, info_.sensors[0].state_interfaces[i].name, &hw_sensor_states_orientation_z));
-            }
-            if (info_.sensors[0].state_interfaces[i].name.find("orientation.w") != std::string::npos)
-            {
-                state_interfaces.emplace_back(hardware_interface::StateInterface(
-                    info_.sensors[0].name, info_.sensors[0].state_interfaces[i].name, &hw_sensor_states_orientation_w));
-            }
+            // Publish the received message to ROS topic
+            auto message = std_msgs::msg::String();
+            message.data =  bot.toString();
+            publisher_->publish(message);
         }
 
-        return state_interfaces;
+        yarp::os::Bottle* bc = p_cmd.read();
+        
     }
 
-    std::vector<hardware_interface::CommandInterface> RobotControllerHardware::export_command_interfaces()
+    void receive_odometry()
     {
-        std::vector<hardware_interface::CommandInterface> command_interfaces;
-
-        for (auto i = 0u; i < info_.joints.size(); i++)
+        yarp::os::Bottle* bt = p_tou.read(false);
+        if (bt != nullptr)
         {
-            if (info_.joints[i].name.find("left") != std::string::npos)
-                command_interfaces.emplace_back(hardware_interface::CommandInterface(
-                    info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &hw_commands_left));
-            if (info_.joints[i].name.find("right") != std::string::npos)
-                command_interfaces.emplace_back(hardware_interface::CommandInterface(
-                    info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &hw_commands_right));
-        }
-        return command_interfaces;
-    }
+            std::vector<double> tou(4);
+            for(int i = 0; i < tou.size(); i++)
+            {
+                tou[i] = bt->get(i).asFloat64();
+            }
+            // Extract the values from the bottle
+            double vx = tou[0];
+            double vy = tou[1];
+            double w  = tou[2];
+            double ta = tou[3];
 
-    hardware_interface::CallbackReturn RobotControllerHardware::on_activate(
-        const rclcpp_lifecycle::State & /*previous_state*/)
-    {
-        RCLCPP_INFO(rclcpp::get_logger("RobotControllerHardware"), "Successfully activated!");
-        return hardware_interface::CallbackReturn::SUCCESS;
-    }
-
-    hardware_interface::CallbackReturn RobotControllerHardware::on_deactivate(
-        const rclcpp_lifecycle::State & /*previous_state*/)
-    {
-        RCLCPP_INFO(rclcpp::get_logger("RobotControllerHardware"), "Successfully deactivated!");
-        return hardware_interface::CallbackReturn::SUCCESS;
-    }
-
-    hardware_interface::return_type RobotControllerHardware::read(
-        const rclcpp::Time & /*time*/, const rclcpp::Duration &period)
-    {
-        try
-        {
-            auto sensor = hardware_handler->getSensor();
-            hw_velocities_left = sensor.velocity.left;
-            hw_velocities_right = sensor.velocity.right;
-            hw_positions_left = hw_positions_left + period.seconds() * sensor.velocity.left;
-            hw_positions_right = hw_positions_right + period.seconds() * sensor.velocity.right;
-
-            hw_sensor_states_accel_x = sensor.imu.accelerometer.x / 1000 * 9.80665;
-            hw_sensor_states_accel_y = sensor.imu.accelerometer.y / 1000 * 9.80665;
-            hw_sensor_states_accel_z = sensor.imu.accelerometer.z / 1000 * 9.80665;
-            // convert to radian/s from degree/s
-            hw_sensor_states_gyro_x = sensor.imu.gyroscope.x * M_PI / 180;
-            hw_sensor_states_gyro_y = sensor.imu.gyroscope.y * M_PI / 180;
-            hw_sensor_states_gyro_z = sensor.imu.gyroscope.z * M_PI / 180;
-
-            // Calculate the roll and pitch angles (in radians)
-            double roll = atan2(hw_sensor_states_accel_y, hw_sensor_states_accel_z);
-            double pitch = atan2(-hw_sensor_states_accel_x, sqrt(hw_sensor_states_accel_y * hw_sensor_states_accel_y + hw_sensor_states_accel_z * hw_sensor_states_accel_z));
-            double yaw = atan2(sensor.imu.magnetometer.y / 1e6, sensor.imu.magnetometer.z / 1e6);
-
-            tf2::Quaternion q;
-            q.setRPY(roll, pitch, yaw);
-            q.normalize();
-            hw_sensor_states_orientation_x = q.x();
-            hw_sensor_states_orientation_y = q.y();
-            hw_sensor_states_orientation_z = q.z();
-            hw_sensor_states_orientation_w = q.w();
-            return hardware_interface::return_type::OK;
-        }
-        catch (const std::exception &e)
-        {
-            RCLCPP_ERROR(rclcpp::get_logger("RobotControllerHardware"), "Error writing to hardware: %s", e.what());
-            return hardware_interface::return_type::ERROR;
+            // Publish the odometry message
+            auto odom = nav_msgs::msg::Odometry();
+            odom.pose.pose.position.x = vx;
+            odom.pose.pose.position.y = vy;
+            odom.pose.pose.orientation.z = w;
+            //odom.pose.pose.orientation.w = ta;
+            odom_publisher_->publish(odom);
         }
     }
 
-    hardware_interface::return_type robot_controller::RobotControllerHardware::write(
-        const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
+    void cmd_callback(const geometry_msgs::msg::Twist::SharedPtr msg)
     {
-        try
+        // Extract the required values
+        double vx = msg->linear.x;
+        double vy = 0;  //msg->linear.y;
+        double bw = msg->angular.z;
+        double ta = 0;  //msg->angular.x;
+
+        // Create a vector with the extracted values
+        std::vector<double> cmd = {vx, vy, bw, ta};
+
+        // Prepare the YARP bottle
+        yarp::os::Bottle& bc = p_cmd.prepare();
+        bc.clear();
+        for (int i = 0; i < cmd.size(); i++)
         {
-            hardware_handler->setVelocity({hw_commands_left, hw_commands_right});
-            return hardware_interface::return_type::OK;
+            bc.addFloat64(cmd[i]);
         }
-        catch (const std::exception &e)
-        {
-            RCLCPP_ERROR(rclcpp::get_logger("RobotControllerHardware"), "Error writing to hardware: %s", e.what());
-            return hardware_interface::return_type::ERROR;
-        }
+        p_cmd.write();
     }
 
-    void RobotControllerHardware::stop()
+    yarp::os::Port port;
+    yarp::os::BufferedPort<yarp::os::Bottle> p_cmd; //motor command
+    yarp::os::BufferedPort<yarp::os::Bottle> p_tou; //encoder reading
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_publisher_;      //odometry
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_subscriber_; //cmd_vel
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<YarpReceiver>();
+
+    rclcpp::Rate rate(20);
+    while(rclcpp::ok())
     {
-        //@TODO: Update implementation when there is an update to controller manager.
-        // Implement workaround to handle ctrl-c termination.
-        // Refer to issue - https://github.com/ros-controls/ros2_control/issues/472
-        RCLCPP_INFO(rclcpp::get_logger("RobotControllerHardware"), "ctrl-c called, shutting down...");
-        hardware_handler->stop();
+        rclcpp::spin_some(node);
+        rate.sleep();
     }
 
+    node->close_ports();
+    rclcpp::shutdown();
+    return 0;
 }
-#include "pluginlib/class_list_macros.hpp"
-PLUGINLIB_EXPORT_CLASS(
-    robot_controller::RobotControllerHardware, hardware_interface::SystemInterface)
