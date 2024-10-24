a78918c55a5bf2f3735edfb09882c7a7f7d8a740
Haruki Isono <haruki.isono861@gmail.com>
2024-10-25 07:55:27 +0900

    add: Odometry

--- hardware/controller.cpp
+++ hardware/controller.cpp
@@ -41,48 +41,73 @@ int main(int argc, char * argv[])
 
 	// Open YARP port
 	p_cmd.open("/remoteController/command:o");  //motor command
-	p_enc.open("/remoteController/encoder:i");  //encoder reading
+	p_enc.open("/remoteController/encoder:i");  //encoder
 
     // Connect to the sender port
     yarp::os::Network::connect("/remoteController/command:o","/vehicleDriver/remote:i");    //motor command
-    yarp::os::Network::connect("/vehicleDriver/encoder:o", "/remoteController/encoder:i");  //encoder reading
+    yarp::os::Network::connect("/vehicleDriver/encoder:o", "/remoteController/encoder:i");  //encoder
 
     /*----------ROS2 Initialize----------*/
     rclcpp::init(argc, argv);   //Node initialize
     auto node = rclcpp::Node::make_shared("lucia_controller"); //Node create
     //create publisher
-    
+    auto odom_publisher = node->create_publisher<nav_msgs::msg::Odometry>("/odom", 10);
     
     //create subscriber
     auto velocity_subscriber = node->create_subscription<geometry_msgs::msg::Twist>("/cmd_vel", 10, velocity_callback);
 
+    //Odom
+    double x = 0.0;
+    double y = 0.0;
+    double theta = 0.0;
 
     rclcpp::WallRate loop_rate(100); //loop rate 100Hz
    	while(rclcpp::ok())
     {
         // Motor command
         rclcpp::spin_some(node);
-        //velocity_callback;
+        
         // Read encoder
-        /*yarp::os::Bottle* bt = p_enc.read(false);
+        yarp::os::Bottle* bt = p_enc.read(false);
         std::vector<double> enc(4);
         if (bt != nullptr)
         {
             for(int i = 0; i < enc.size(); i++){
                 enc[i] = bt->get(i).asFloat64();
             }
-        }*/
+        }
         
-        /*std::cout << "enc " 
-            << enc[0] << " " 
-            << enc[1] << " " 
-            << enc[2] << " " 
-            << enc[3] << " " 
-            << "cmd " 
-            << cmd[0] << " " 
-            << cmd[1] << " " 
-            << cmd[2] << " " 
-            << cmd[3] << std::endl;*/
+        // caluculate odometry
+        double vx = enc[0];
+        double vy = enc[1];
+        double vth = enc[2];
+
+        double dt = 0.01;   //loop rate 100Hz
+        double delta_x = vx * dt;
+        double delta_y = vy * dt;
+        double delta_th = vth * dt;
+
+        x += delta_x;
+        y += delta_y;
+        theta += delta_th;
+
+        // Publish odometry
+        auto odom_msg = nav_msgs::msg::Odometry();
+        odom_msg.header.stamp = node->now();
+        odom_msg.header.frame_id = "odom";
+
+        odom_msg.pose.pose.position.x = x;
+        odom_msg.pose.pose.position.y = y;
+        odom_msg.pose.pose.position.z = 0.0;
+        odom_msg.pose.pose.orientation.z = sin(theta / 2.0);
+        odom_msg.pose.pose.orientation.w = cos(theta / 2.0);
+
+        odom_msg.child_frame_id = "base_link";
+        odom_msg.twist.twist.linear.x = vx;
+        odom_msg.twist.twist.linear.y = vy;
+        odom_msg.twist.twist.angular.z = vth;
+
+        odom_publisher->publish(odom_msg);
 
         yarp::os::Time::delay(0.05);
         loop_rate.sleep();  //coodinate with loop_rate
@@ -90,5 +115,6 @@ int main(int argc, char * argv[])
 
     p_cmd.close();
     p_enc.close();
+    rclcpp::shutdown();
 	return 0;
 }
