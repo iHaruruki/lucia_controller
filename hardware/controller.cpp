a667207759d61ac11a65f59872eb8515a6c2090c
Haruki Isono <haruki.isono861@gmail.com>
2024-10-25 16:57:00 +0900

    add: hardware/controller.cpp

--- hardware/controller.cpp
+++ hardware/controller.cpp
@@ -1,94 +1,121 @@
-#include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
-#include <std_msgs/msg/float64_multi_array.hpp>
-#include <geometry_msgs/msg/twist.hpp>
-#include <nav_msgs/msg/odometry.hpp>
-#include <yarp/os/all.h>
-#include <yarp/os/Network.h>
-#include <yarp/os/Port.h>
-#include <yarp/os/Bottle.h>
+#include "robot_controller/hardwareHandler.hpp"
+
+#include <chrono>
+#include <cmath>
+#include <cstddef>
+#include <limits>
+#include <memory>
 #include <vector>
-#include <iostream>
+#include <tf2/LinearMath/Quaternion.h>
 
-// YARP port
-yarp::os::BufferedPort<yarp::os::Bottle> p_cmd; //motor command
-yarp::os::BufferedPort<yarp::os::Bottle> p_enc; //encoder reading
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp/logging.hpp"
 
-
-void velocity_callback(const geometry_msgs::msg::Twist::SharedPtr msg)
+namespace robot_controller
 {
-    std::vector<double> cmd(4);
-    cmd[0]=msg->linear.x;
-    cmd[1]=0.00;
-    cmd[2]=msg->angular.z;
-    cmd[3]=0.00;
-
-    yarp::os::Bottle& bc = p_cmd.prepare();
-    bc.clear();
-    for(int i = 0; i < cmd.size(); i++){
-       bc.addFloat64(cmd[i]);
-    }
-    p_cmd.write();
-
-    //RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Received velocity command: linear.x=%f, angular.z=%f", msg->linear.x, msg->angular.z);
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Send velocity command: linear.x=%f, angular.z=%f", cmd[0], cmd[2]);
-}
-
-int main(int argc, char * argv[])
-{
-    /*----------YARP Initialize----------*/
-	yarp::os::Network yarp;
-
-	// Open YARP port
-	p_cmd.open("/remoteController/command:o");  //motor command
-	p_enc.open("/remoteController/encoder:i");  //encoder reading
-
-    // Connect to the sender port
-    yarp::os::Network::connect("/remoteController/command:o","/vehicleDriver/remote:i");    //motor command
-    yarp::os::Network::connect("/vehicleDriver/encoder:o", "/remoteController/encoder:i");  //encoder reading
-
-    /*----------ROS2 Initialize----------*/
-    rclcpp::init(argc, argv);   //Node initialize
-    auto node = rclcpp::Node::make_shared("lucia_controller"); //Node create
-    //create publisher
-    
-    
-    //create subscriber
-    auto velocity_subscriber = node->create_subscription<geometry_msgs::msg::Twist>("/cmd_vel", 10, velocity_callback);
-
-
-    rclcpp::WallRate loop_rate(100); //loop rate 100Hz
-   	while(rclcpp::ok())
+    hardware_interface::CallbackReturn robot_controllerHardware::on_init(
+        const hardware_interface::HardwareInfo & info)
     {
-        // Motor command
-        rclcpp::spin_some(node);
-        //velocity_callback;
-        // Read encoder
-        /*yarp::os::Bottle* bt = p_enc.read(false);
-        std::vector<double> enc(4);
-        if (bt != nullptr)
+        if(hardware_interface::SystemInterface::on_init(info) != hardware_interface::CallbackRetturn::SUCCESS)
         {
-            for(int i = 0; i < enc.size(); i++){
-                enc[i] = bt->get(i).asFloat64();
-            }
-        }*/
+            return hardware_interface::CallbackReturn::ERROR;
+        }
         
-        /*std::cout << "enc " 
-            << enc[0] << " " 
-            << enc[1] << " " 
-            << enc[2] << " " 
-            << enc[3] << " " 
-            << "cmd " 
-            << cmd[0] << " " 
-            << cmd[1] << " " 
-            << cmd[2] << " " 
-            << cmd[3] << std::endl;*/
+        //port = info.hardware_parameters["YARP_port"];
+        //RCLLCPP_INFO(rclcpp::get_logger("robot_controllerHardware"), "Hardware parameters: %s", port.c_str());
+        rcllcpp::on_shutdown(&robot_controllerHardware::stop, this);
+        return hardware_interface::CallbackReturn::SUCCESS;
+    }
 
-        yarp::os::Time::delay(0.05);
-        loop_rate.sleep();  //coodinate with loop_rate
-    }	
+    hardware_interface::CallbackReturn robot_controllerHardware::on_configure(
+        const rclcpp_lifecycle::State & /*previous_state*/)
+    {
+        //hardware_handler = new HardwareHandler(port);
+        //hardware_handler->init();
+        //RCLL_INFO(rclcpp::get_logger("robot_controllerHardware"), "Successfully configured!");
+        return hardware_interface::CallbackReturn::SUCCESS;
+    }
 
-    p_cmd.close();
-    p_enc.close();
-	return 0;
+    /*std::vector<hardware_interface::StateInterface> robot_controllerHardware::export_state_interfaces()
+    {
+        std::vector<hardware_interface::StateInterface> state_interfaces;
+    }*/
+
+   //export sensor state interface
+   /*for(unit i = 0; i < info_.sensors[0].state_interfaces.size(); i++)
+   {
+       state_interfaces.emplace_back(info_.sensors[0].state_interfaces[i].name, info_.sensors[0].state_interfaces[i].type, &hw_positions_left);
+   }*/
+
+    std::vector<hardware_interface::CommandInterface> robot_controllerHardware::export_command_interface()
+    {
+        std::vector<hardware_interface::CommandInterface> command_interfaces;
+
+        for(auto i = 0; i < info_.joints.size(); i++)
+        {
+            command_interfaces.emplace_back("left_wheel", hardware_interface::HW_IF_VELOCITY, &hw_commands_left);
+            command_interfaces.emplace_back("right_wheel", hardware_interface::HW_IF_VELOCITY, &hw_commands_right);
+        }
+        return command_interfaces;
+    }
+
+    hardware_interface::CallbackReturn robot_controllerHardware::on_activate(
+        const rclcpp_lifecycle::State & /*previous_state*/)
+    {
+        RCLCPP_INFO(rclcpp::get_logger("robot_controllerHardware"), "Successfully activated!");
+        return hardware_interface::CallbackReturn::SUCCESS;
+    }
+
+    hardware_interface::CallbackReturn_type robot_controllerHardware::on_deactivate(
+        const rclcpp_lifecycle::State & /*previous_state*/)
+    {
+        RCLCPP_INFO(rclcpp::get_logger("robot_controllerHardware"), "Successfully deactivated!");
+        return hardware_interface::CallbackReturn::SUCCESS;
+    }
+
+    hardware_interface::CallbackReturn robot_controllerHardware::read(
+        const rclcpp::Time & /*time*/, const rclcpp::Duration &period)
+    {
+        auto sensor = hardware_handler->getSensor();
+        hw_velocities_left = sensor.velocity.left;
+        hw_velocities_right = sensor.velocity.right;
+        hw_positions_left = hw_positions_left + period.seconds() * sensor.velocity.left;
+        hw_positions_right = hw_positions_right + period.seconds() * sensor.velocity.right;
+
+        //Calculate the roll and pitch angles(in radians)
+        //double roll = atan2();
+        //double pitch = atan2();
+        //double yaw = atan2();
+
+        tf2::Quaternion q;
+        q.setRPY(roll, pitch, yaw);
+        q.normalize();
+        hw_sensoe_state.orientation.x = q.x();
+        hw_sensoe_state.orientation.y = q.y();
+        hw_sensoe_state.orientation.z = q.z();
+        hw_sensoe_state.orientation.w = q.w();
+        return hardware_interface::return_type::OK;
+
+        /*catch (const std::exception &e)
+        {
+            RCLCPP_ERROR(rclcpp::get_logger("robot_controllerHardware"), "Error reading sensor data: %s", e.what());
+            return hardware_interface::CallbackReturn::ERROR;
+        }*/
+    }
+
+    hardware_interface::return_type robot_controllerHardware::write(
+        const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
+    {
+        wheelVelocity velocity;
+        velocity.left = hw_commands_left;
+        velocity.right = hw_commands_right;
+        hardware_handler->setVelocity(velocity);
+        return hardware_interface::return_type::OK;
+    }
+
+    void robot_controllerHardware::stop()
+    {
+        RCLLCPP_INFO(rclcpp::get_logger("robot_controllerHardware"), "ctl+c detected, shutting down...");
+        hardware_handler->stop();
+    }
 }
