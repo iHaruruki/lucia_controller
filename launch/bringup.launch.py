afd58f2df4f8b7df7ca10703346abeb1bedee1e8
HarkiIsono <haruki.isono861@gmail.com>
2025-06-06 14:48:19 +0900

    fix

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -2,34 +2,31 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    package_name = 'lucia_description'  # package name
-    urdf_file_name = 'lucia.urdf'    # URDF file name
+    # パッケージのshareディレクトリの取得
+    lucia_description_pkg = get_package_share_directory('lucia_description')
+    lucia_controller_pkg = get_package_share_directory('lucia_controller')
+    urg_node2_pkg = get_package_share_directory('urg_node2')
+    dual_laser_merger_pkg = get_package_share_directory('dual_laser_merger')
 
-    # パッケージの共有ディレクトリを取得
-    urdf_file_path = os.path.join(
-        get_package_share_directory(package_name),
-        'urdf',
-        urdf_file_name
+    # launchファイルへのフルパス
+    description = os.path.join(lucia_description_pkg, 'launch', 'robot.launch.py')
+    controller = os.path.join(lucia_controller_pkg, 'launch', 'controller.launch.py')
+    urg_node2 = os.path.join(urg_node2_pkg, 'launch', 'urg_node2_2lidar.launch.py')
+    dual_laser_merger = os.path.join(dual_laser_merger_pkg, 'launch', 'urg_node2_merger.launch.py')
+
+    # IncludeLaunchDescription
+    include_description = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(description)
     )
 
-    # URDFの内容を取得
-    with open(urdf_file_path, 'r') as infp:
-        robot_desc = infp.read()
+    ld = LaunchDescription()
+    ld.add_action(description)
+    ld.add_action(controller)
+    ld.add_action(urg_node2)
+    ld.add_action(dual_laser_merger)
 
-    return LaunchDescription([
-        Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            name='joint_state_publisher',
-            output='screen',
-        ),
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_pushlisher',
-            output='screen',
-            parameters=[{'robot_description': robot_desc}]
-        )
-])
+    return ld
