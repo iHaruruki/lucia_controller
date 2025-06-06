4c7e388f8605184a612e3480d2e1843629179ab8
HarkiIsono <haruki.isono861@gmail.com>
2025-06-06 21:57:19 +0900

    fix

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -7,7 +7,6 @@ from launch.actions import IncludeLaunchDescription
 
 def generate_launch_description():
 
-    ld = LaunchDescription()
     # パッケージのshareディレクトリの取得
     lucia_description_pkg = get_package_share_directory('lucia_description')
     lucia_controller_pkg = get_package_share_directory('lucia_controller')
@@ -18,7 +17,7 @@ def generate_launch_description():
     description_path = os.path.join(lucia_description_pkg, 'launch', 'robot.launch.py')
     controller_path = os.path.join(lucia_controller_pkg, 'launch', 'controller.launch.py')
     urg_node2_path = os.path.join(urg_node2_pkg, 'launch', 'urg_node2_2lidar.launch.py')
-    dual_laser_merger_path = os.path.join(dual_laser_merger_pkg, 'launch', 'urg_node2_merger.launch.py')
+    dual_laser_merger_path = os.path.join(dual_laser_merger_pkg, 'launch', 'demo_laser_merger.launch.py')
 
     # IncludeLaunchDescription オブジェクトを作成
     include_description = IncludeLaunchDescription(
@@ -31,16 +30,16 @@ def generate_launch_description():
     )
 
     include_urg_node2 = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(urg_node2_path)
+        PythonLaunchDescriptionSource(urg_node2_path),
         launch_arguments={'use_sim_time': 'false'}.items()
     )
 
     include_dual_laser_merger = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(dual_laser_merger_path)
+        PythonLaunchDescriptionSource(dual_laser_merger_path),
         launch_arguments={'use_sim_time': 'false'}.items()
     )
 
-    # corrected: 文字列ではなく IncludeLaunchDescription オブジェクトを渡す
+    ld = LaunchDescription()
     ld.add_action(include_description)
     ld.add_action(include_controller)
     ld.add_action(include_urg_node2)
