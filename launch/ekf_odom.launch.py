3dbb481eae79a33dd660c351a9b86c8f8a0cffa5
HarukiIsono <haruki.isono861@gmail.com>
2025-09-08 23:48:28 +0900

    fix

--- launch/ekf_odom.launch.py
+++ launch/ekf_odom.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    pkg_share = get_package_share_directory('lucia_controller')  # 例: パッケージ名
+    pkg_share = get_package_share_directory('lucia_controller')
     ekf_path = os.path.join(pkg_share, 'config', 'ekf_odom.yaml')
 
     return LaunchDescription([
@@ -17,22 +17,14 @@ def generate_launch_description():
         ),
         Node(
             package='lucia_controller',
-            executable='lucia_controller_node',
+            executable='lucia_controller_node',   # 実際のターゲット名に合わせる
             name='lucia_controller',
             parameters=[
                 {"publish_tf": False},
                 {"raw_odom_topic": "wheel_odom"},
                 {"raw_odom_frame": "wheel_odom"},
-                {"base_link_frame": "base_link"},
-                {"use_smoothing": True},
-                {"use_ramp": True}
+                {"base_link_frame": "base_link"}
             ],
             output='screen'
-        ),
-        # base_link と base_footprint を分けたい場合:
-        # Node(
-        #   package="tf2_ros",
-        #   executable="static_transform_publisher",
-        #   arguments=["0","0","0","0","0","0","base_link","base_footprint"]
-        # )
+        )
     ])
