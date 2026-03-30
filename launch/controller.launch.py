d85004871c089b07583b3b1c54cea3cb8983568f
HarukiIsono <haruki.isono861@gmail.com>
2026-03-30 15:46:51 +0900

    fix

--- launch/controller.launch.py
+++ launch/controller.launch.py
@@ -16,29 +16,6 @@ def generate_launch_description():
             name='lucia_controller_node',
             output='screen',
         ),
-
-        # Velocity Smoother
-        Node(
-            package='nav2_velocity_smoother',
-            executable='velocity_smoother',
-            name='lucia_velocity_smoother',
-            parameters=[
-                {
-                    'smoothing_frequency': 100.0,
-                    'scale_max_vel': 1.0,
-                    'scale_max_angular_vel': 1.0,
-                    'max_velocity': [1.0, 1.0],
-                    'max_angular_velocity': 2.0,
-                    'deadband_velocity': [0.0, 0.0],
-                    'deadband_angular_velocity': 0.0,
-                    'max_accel': [1.0, 1.0],
-                    'max_decel': [1.0, 1.0],
-                    'max_angular_accel': 2.0,
-                    'max_angular_decel': 2.0,
-                }
-            ],
-            output='screen',
-        ),
         
         # lucia_trajectory_drawer_node
         Node(
