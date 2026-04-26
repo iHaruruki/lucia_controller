2ad13663ca461a9c22ad074475a3860c993ff925
HarukiIsono <haruki.isono861@gmail.com>
2026-04-26 21:38:28 +0900

    choro

--- src/lucia_controller.cpp
+++ src/lucia_controller.cpp
@@ -189,25 +189,25 @@ void LuciaController::publishOdometry(const rclcpp::Time& stamp, double vx, doub
     odom.twist.twist.angular.z = vth;
 
     // Covariance
-    for (int i = 0; i < 36; i++) {
-        odom.pose.covariance[i] = 0.0;
-        odom.twist.covariance[i] = 0.0;
-    }
-    // Pose
-    odom.pose.covariance[0] = 0.02;   // x
-    odom.pose.covariance[7] = 0.02;   // y
-    odom.pose.covariance[14] = 1e6;   // z
-    odom.pose.covariance[21] = 1e6;   // roll
-    odom.pose.covariance[28] = 1e6;   // pitch
-    odom.pose.covariance[35] = 0.5;   // yaw
+    // for (int i = 0; i < 36; i++) {
+    //     odom.pose.covariance[i] = 0.0;
+    //     odom.twist.covariance[i] = 0.0;
+    // }
+    // // Pose
+    // odom.pose.covariance[0] = 0.02;   // x
+    // odom.pose.covariance[7] = 0.02;   // y
+    // odom.pose.covariance[14] = 1e6;   // z
+    // odom.pose.covariance[21] = 1e6;   // roll
+    // odom.pose.covariance[28] = 1e6;   // pitch
+    // odom.pose.covariance[35] = 0.02;   // yaw
 
-    // Twist
-    odom.twist.covariance[0] = 0.01;  // vx
-    odom.twist.covariance[7] = 0.01;  // vy
-    odom.twist.covariance[14] = 1e6;  // vz
-    odom.twist.covariance[21] = 1e6;  // vroll
-    odom.twist.covariance[28] = 1e6;  // vpitch
-    odom.twist.covariance[35] = 0.5;  // vth
+    // // Twist
+    // odom.twist.covariance[0] = 0.01;  // vx
+    // odom.twist.covariance[7] = 0.01;  // vy
+    // odom.twist.covariance[14] = 1e6;  // vz
+    // odom.twist.covariance[21] = 1e6;  // vroll
+    // odom.twist.covariance[28] = 1e6;  // vpitch
+    // odom.twist.covariance[35] = 0.02;  // vth
 
     odom_publisher_->publish(odom);
 
