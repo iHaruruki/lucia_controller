1c43a6f58cd3f0e30b3210c0e9f74280478a759b
Haruki Isono <haruki.isono861@gmail.com>
2026-09-03 20:51:19 +0900

    wip

--- launch/ekf_localization.launch.py
+++ launch/ekf_localization.launch.py
@@ -4,15 +4,10 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    # lucia_controller_dir = get_package_share_directory('lucia_controller')
-    # default_ekf_path = os.path.join(lucia_controller_dir, 'config', 'ekf.yaml')
-    # os.environ['FILE_PATH'] = str(default_ekf_path)
-
-    # Declare launch arguments
+    # complementary filter args
     complementary_args = [
         DeclareLaunchArgument("cf_publish_tf", default_value="true"),
         DeclareLaunchArgument("cf_use_mag", default_value="false"),
@@ -21,18 +16,20 @@ def generate_launch_description():
         DeclareLaunchArgument("cf_gain_mag", default_value="0.01"),
     ]
 
-    ekf_arg = [
-        DeclareLaunchArgument(
-            "ef_ekf_yaml",
-            default_value=PathJoinSubstitution([
-                FindPackageShare('lucia_controller'),
-                'config',
-                'ekf.yaml'
-            ]),
-            description="Path to EKF parameter file"
-        ),
+    ekf_yaml_arg = DeclareLaunchArgument(
+        "ef_ekf_yaml",
+        default_value=PathJoinSubstitution([
+            FindPackageShare('lucia_controller'),
+            'config',
+            'ekf.yaml'
+        ]),
+        description="Path to EKF parameter file"
+    )
+
+    # Optional topic override args
+    ekf_topic_args = [
         DeclareLaunchArgument("ef_odom0", default_value="/odom"),
-        DeclareLaunchArgument("ef_imu0", default_value="/imu/data"),
+        DeclareLaunchArgument("ef_imu0", default_value="/imu/data"),  # CF出力に合わせる
     ]
 
     cf_parameters = [{
@@ -43,18 +40,14 @@ def generate_launch_description():
         "gain_mag": LaunchConfiguration("cf_gain_mag"),
     }]
 
-    ekf_parameters = [{
-        "ekf_yaml": LaunchConfiguration("ef_ekf_yaml"),
-        "odom0": LaunchConfiguration("ef_odom0"),
-        "imu0": LaunchConfiguration("ef_imu0"),
-    }]
-
     complementary_filter_node = Node(
         package='imu_complementary_filter',
         executable='complementary_filter_node',
         name='complementary_filter_node',
         parameters=cf_parameters,
-        remappings=[('/imu/data_raw', '/camera/gyro_accel/sample')],
+        remappings=[
+            ('/imu/data_raw', '/camera/gyro_accel/sample'),
+        ],
         output='screen',
     )
 
@@ -62,15 +55,18 @@ def generate_launch_description():
         package='robot_localization',
         executable='ekf_node',
         name='ekf_node',
-        parameters=ekf_parameters,
-        remappings=[
-            # ('/imu/data_raw', '/camera/gyro_accel/sample'),
+        parameters=[
+            LaunchConfiguration("ef_ekf_yaml"),
+            {
+                "odom0": LaunchConfiguration("ef_odom0"),
+                "imu0": LaunchConfiguration("ef_imu0"),
+            }
         ],
         output='screen',
     )
 
     return LaunchDescription(
-        complementary_args + ekf_arg + [
+        complementary_args + [ekf_yaml_arg] + ekf_topic_args + [
             complementary_filter_node,
             ekf_node,
         ]
